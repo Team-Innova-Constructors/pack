4b54c3c60dd150285ac7fef3d9d92846eb42f2ba
lovelyfirefly <1494932845@qq.com>
2024-10-23 04:01:46 +0800

    坚不可摧的石墨电极

--- kubejs/server_scripts/l2.js
+++ kubejs/server_scripts/l2.js
@@ -1,21 +1,16 @@
 ServerEvents.recipes(event => {
 
-event.shaped(Item.of('l2hostility:curse_of_wrath',1), [
+  event.shaped(Item.of('l2hostility:curse_of_wrath', 1), [
     'ACA',
     'DBD',
     'AEA'
   ],
-  {
-    A: 'minecraft:iron_ingot',
-    B: 'l2hostility:chaos_ingot',
-    C: 'l2hostility:counter_strike',
-    D: 'l2complements:cursed_droplet',
-    E: 'l2hostility:nausea'
-  }
+    {
+      A: 'minecraft:iron_ingot',
+      B: 'l2hostility:chaos_ingot',
+      C: 'l2hostility:counter_strike',
+      D: 'l2complements:cursed_droplet',
+      E: 'l2hostility:nausea'
+    }
   )
-
-
-
-
-
 })
