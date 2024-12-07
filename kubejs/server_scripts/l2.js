db49123a74247eb62e237a45f15cd48c636e1f44
EtSH-C2H6S <2130679254@qq.com>
2024-12-07 20:27:32 +0800

    配方修改、思索、模组更新

--- kubejs/server_scripts/l2.js
+++ kubejs/server_scripts/l2.js
@@ -1,16 +1,3 @@
 ServerEvents.recipes(event => {
 
-  event.shaped(Item.of('l2hostility:curse_of_wrath', 1), [
-    'ACA',
-    'DBD',
-    'AEA'
-  ],
-    {
-      A: 'minecraft:iron_ingot',
-      B: 'l2hostility:chaos_ingot',
-      C: 'l2hostility:blindness',
-      D: 'l2complements:cursed_droplet',
-      E: 'l2hostility:nausea'
-    }
-  )
 })
